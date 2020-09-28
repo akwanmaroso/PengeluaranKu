0bef9778e3a08864e74dd256f4c4b9bb11baea63
akwanmaroso <akwanmaroso@gmail.com>
2020-09-29 00:07:34 +0800

    add config file

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// DbDriver is name for driver db
+	DbDriver = ""
+	// DbURL is url to connect db
+	DbURL = ""
+)
+
+// Load configuration file
+func Load() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DbDriver = os.Getenv("DB_DRIVER")
+	DbURL = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+}
