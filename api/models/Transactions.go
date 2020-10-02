fa3ec48547ae147888bc77862f42b416e3728aba
akwanmaroso <akwanmaroso@gmail.com>
2020-10-02 20:55:50 +0800

    change json name

--- api/models/Transactions.go
+++ api/models/Transactions.go
@@ -6,7 +6,7 @@ type Transaction struct {
 	ID          uint32    `gorm:"primary_key;auto_increment" json:"id"`
 	Date        time.Time `gorm:"default:current_timestamp()" json:"date"`
 	Amount      uint64    `gorm:"int;size:12; not null" json:"amount"`
-	InOut       bool      `gorm:"boolean;not null" json:"int_out"`
+	InOut       bool      `gorm:"boolean;not null" json:"in_out"`
 	Description string    `gorm:"text" json:"description"`
 	Category    Category  `json:"category"`
 	CategoryID  uint32    `json:"category_id"`
