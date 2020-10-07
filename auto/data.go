8a0ed5dc789ec676829c2da9172a9dae57a16237
akwanmaroso <akwanmaroso@gmail.com>
2020-10-07 18:30:36 +0800

    update load data

--- auto/data.go
+++ auto/data.go
@@ -5,8 +5,8 @@ import "github.com/akwanmaroso/PengeluaranKu/api/models"
 var users = []models.User{
 	{
 		Name:     "JohnDoe",
-		Email:    "jhondoe@email.com",
-		Password: "123456789",
+		Email:    "test@email.com",
+		Password: "test",
 	},
 }
 
