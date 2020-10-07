0863f7392c2cf148aba21913dcf3348d81249f85
akwanmaroso <akwanmaroso@gmail.com>
2020-10-07 18:33:53 +0800

    feat: add method check validToken, extractToken, and extractTokenById

--- api/auth/token.go
+++ api/auth/token.go
@@ -1,8 +1,14 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/akwanmaroso/PengeluaranKu/config"
+	"github.com/akwanmaroso/PengeluaranKu/utils/console"
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
@@ -12,5 +18,68 @@ func CreateToken(user_id uint32) (string, error) {
 	claims["user_id"] = user_id
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte("mysecretsecretkey"))
+	return token.SignedString(config.SECRET_KEY)
+}
+
+func TokenValid(r *http.Request) error {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return config.SECRET_KEY, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println("")
+		console.Pretty(claims)
+	}
+	return nil
+}
+
+func ExtractToken(r *http.Request) string {
+	keys := r.URL.Query()
+	token := keys.Get("token")
+	if token != "" {
+		return token
+	}
+
+	bearerToken := r.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func ExtractTokenID(r *http.Request) (uint32, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return config.SECRET_KEY, nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(
+			fmt.Sprintf("%0.f", claims["user_id"]), 10, 32,
+		)
+
+		if err != nil {
+			return 0, err
+		}
+
+		return uint32(uid), nil
+	}
+	return 0, nil
 }
