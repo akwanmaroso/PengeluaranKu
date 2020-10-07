6bc098a89cc18d2f7cead7bc13958897aad500fd
akwanmaroso <akwanmaroso@gmail.com>
2020-10-07 18:32:39 +0800

    add env SECRET_KEY

--- config/config.go
+++ config/config.go
@@ -13,7 +13,8 @@ var (
 	// DbDriver is name for driver db
 	DbDriver = ""
 	// DbURL is url to connect db
-	DbURL = ""
+	DbURL      = ""
+	SECRET_KEY []byte
 )
 
 // Load configuration file
@@ -26,4 +27,6 @@ func Load() {
 
 	DbDriver = os.Getenv("DB_DRIVER")
 	DbURL = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	SECRET_KEY = []byte(os.Getenv("API_SECRET"))
 }
