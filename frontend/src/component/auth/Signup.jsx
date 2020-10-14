dcbbe2faa09f77027e7a8e6685effaa32e845ce5
akwanmaroso <akwanmaroso@gmail.com>
2020-10-14 18:36:14 +0800

    feat: add singup

--- frontend/src/component/auth/Signup.jsx
+++ frontend/src/component/auth/Signup.jsx
@@ -1,6 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {notifError, notifSuccess} from "../../utils/notifications"
 
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    verifyPassword: "",
+
+}
 const Signup = () => {
+    const [state, setState] = useState(initialState)
+
+    const handleChangeEmail = (e) => {
+        setState({
+            ...state,
+            email: e.target.value
+        })
+    }
+    const handleChangeName = (e) => {
+        setState({
+            ...state,
+            name: e.target.value
+        })
+    }
+    const handleChangePassword = (e) => {
+        setState({
+            ...state,
+            password: e.target.value
+        })
+    }
+    const handleVerifyPassword = (e) => {
+        setState({
+            ...state,
+            verifyPassword: e.target.value
+        })
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault()
+        // const {name, password, email, verifyPassword} = state
+        axios.post("http://localhost:9000/users", state)
+            .then(res => {
+                notifSuccess("Signup successfully")
+                setTimeout(() => {
+                    window.location = "/"
+                }, 2000)
+            }).catch(err => {
+            notifError(`${err.message}`)
+        })
+        console.log(state)
+    }
+
+    const validatePassword = () => {
+        return state.password !== state.verifyPassword;
+    }
+
+
     return (
         <div className="d-flex justify-content-center">
             <div className="row">
@@ -16,7 +72,7 @@ const Signup = () => {
                                     id="name"
                                     className="form-control"
                                     name="name"
-                                    // onChange={onChangeEmail}
+                                    onChange={handleChangeName}
                                     required
                                 />
                             </div>
@@ -27,35 +83,42 @@ const Signup = () => {
                                     id="email"
                                     className="form-control"
                                     name="email"
-                                    // onChange={onChangeEmail}
+                                    onChange={handleChangeEmail}
                                     required
                                 />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
                                 <input
-                                    type="text"
+                                    type="password"
                                     id="password"
                                     className="form-control"
                                     name="password"
-                                    // onChange={onChangePassword}
+                                    onChange={handleChangePassword}
                                     required
                                 />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="Vpassword">Verify Password</label>
+                                <label htmlFor="verifyPassword">Verify Password</label>
                                 <input
-                                    type="text"
-                                    id="Vpassword"
+                                    type="password"
+                                    id="verifyPassword"
                                     className="form-control"
-                                    name="Vpassword"
-                                    // onChange={onChangePassword}
+                                    name="verifyPassword"
+                                    onChange={handleVerifyPassword}
                                     required
                                 />
+                                {
+                                    validatePassword() ? (
+                                        <p className="text-danger">Password not match</p>
+                                    ) : (
+                                        ""
+                                    )
+                                }
                             </div>
                             <div className="form-group">
                                 <button
-                                    // onClick={handleLogin}
+                                    onClick={handleSignup}
                                     className="btn btn-success">
                                     SignUp
                                 </button>
