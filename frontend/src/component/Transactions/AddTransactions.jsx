ef1f97d9c230d2531d722a7daa53e31751c22526
akwanmaroso <akwanmaroso@gmail.com>
2020-10-15 21:28:30 +0800

    feat: add category select

--- frontend/src/component/Transactions/AddTransactions.jsx
+++ frontend/src/component/Transactions/AddTransactions.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {notifError, notifSuccess} from "../../utils/notifications";
+import {isAuth} from "../../utils/isAuth";
 
 const initialData = {
     amount: 0,
@@ -12,6 +13,7 @@ const initialData = {
 };
 const AddTransactions = (props) => {
     const [data, setData] = useState(initialData);
+    const [category, setCategory] = useState([])
 
     const handleChangeAmount = (e) => {
         setData({
@@ -27,13 +29,20 @@ const AddTransactions = (props) => {
         });
     };
 
-    const postTransaction = (token) => {
+    const handleChangeCategory = (e) => {
+        setData({
+            ...data,
+            category_id: Number(e.target.value),
+        });
+    };
+
+    const postTransaction = (token, type) => {
         axios
             .post("http://localhost:9000/transactions", data, {
                 headers: {Authorization: token},
             })
             .then(() => {
-                notifSuccess("Add income success");
+                notifSuccess(`Add ${type} success`);
                 setData(initialData);
             })
             .catch((err) => {
@@ -45,25 +54,38 @@ const AddTransactions = (props) => {
     const handleAddIncome = (e) => {
         e.preventDefault();
 
-
-        console.log(data)
         let token = localStorage.getItem("token-user");
         const AuthStr = "Bearer ".concat(token);
-        console.log(data);
-        postTransaction(AuthStr);
+        postTransaction(AuthStr, "income");
         resetForm();
     };
 
     const handleAddSpending = (e) => {
         e.preventDefault();
         data.in_out = true
-        console.log(data)
 
         let token = localStorage.getItem("token-user");
         const AuthStr = "Bearer ".concat(token);
-        postTransaction(AuthStr)
+        postTransaction(AuthStr, "spending")
         resetForm();
     };
+
+    const getCategory = () => {
+        if (isAuth()) {
+            let token = localStorage.getItem("token-user");
+            const AuthStr = "Bearer ".concat(token);
+            axios
+                .get("http://localhost:9000/categories", {
+                    headers: {Authorization: AuthStr},
+                })
+                .then((res) => setCategory(res.data))
+                .catch((err) => console.log(err));
+        }
+    }
+
+    useEffect(() => {
+        getCategory()
+    }, [])
 
     const resetForm = () => {
         document.getElementById("form-transactions").reset();
@@ -76,22 +98,38 @@ const AddTransactions = (props) => {
     return (
         <div>
             <form id="form-transactions">
-                <input
-                    name="amount"
-                    type="number"
-                    className="form-control mb-2"
-                    placeholder="eg. 800000"
-                    onChange={handleChangeAmount}
-                    required
-                />
-                <input
-                    name="description"
-                    type="text"
-                    className="form-control"
-                    placeholder="eg. Buy whiskies"
-                    onChange={handleChangeDesc}
-                    required
-                />
+                <div className="form-group">
+                    <input
+                        name="amount"
+                        type="number"
+                        className="form-control mb-2"
+                        placeholder="eg. 800000"
+                        onChange={handleChangeAmount}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        name="description"
+                        type="text"
+                        className="form-control"
+                        placeholder="eg. Buy whiskies"
+                        onChange={handleChangeDesc}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <select name="category" id="category" className="form-control" onChange={handleChangeCategory}>
+                        {/*<option value="as">Select Category</option>*/}
+                        {
+                            category.map(c => {
+                                return (
+                                    <option key={c.id} value={c.id}>{c.name}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
                 <div className="row mt-3">
                     <div className="col">
                         <button
